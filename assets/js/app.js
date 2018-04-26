798040df88502fd9bcebe9c82cc0a8e33b73699d
Philip <philippernf@gmail.com>
2018-04-26 12:53:34 +0100

    jquery hovers working for house

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
 // alert('JS loaded');
+
+$(() => {
+
+  $('.house').hover(function() {
+    $('body').toggleClass('house-hover');
+  });
+
+});
+
+
+// $('.house').mouseover(function() {
+//   $('body').css('background-image', 'url(./assets/img/placeholder-01.jpg)');
+// }).mouseout(function() {
+//   $('body').css('background-image', 'none');
+// });
+
+
+// $(".result").hover(
+//   function () {
+//     $(this).addClass("result_hover");
+//   },
+//   function () {
+//     $(this).removeClass("result_hover");
+//   }
+// );
